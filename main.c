c78c11904d8616576813af5d35b550cb9ed7e0c1
Jean Carlo Sanchuki Filho <jean.sanchuki@gmail.com>
2021-01-25 15:39:48 -0300

    Init + structs + main

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct node
+{	
+	float value;
+	Node *son;
+
+} Node;
+
+typedef struct actor
+{
+	int groups[];
+	int cost;
+
+} Actor;
+
+
+int main(int argc, char *argv[ ]){
+
+	if(argc > 1)
+		printf("O número de argumentos é maior do que o esperado");
+
+	int AllGroups[];
+
+	return 0;
+}
+
+Actor* startActor(int size){
+
+}
