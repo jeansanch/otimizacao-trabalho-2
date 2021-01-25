f8a6376b4c93aa3583f13354f875faed7471196d
Jean Carlo Sanchuki Filho <jean.sanchuki@gmail.com>
2021-01-25 16:49:04 -0300

    Actor allocation and inputs

--- main.c
+++ main.c
@@ -23,9 +23,8 @@ int main(int argc, char *argv[ ]){
 	//Variaveis
 	bool parameter_a = false;
 	int *all_groups;
-	Actor *actors;
-	int n_grupos = 0, n_atores = 0, n_personagens = 0;
-	
+	int n_groups = 0, n_actors = 0, n_chars = 0;
+	int i;
 	//Verificação de argumentos
 	if(argc > 2)
 		printf("O numero de argumentos e maior do que o esperado");
@@ -39,9 +38,16 @@ int main(int argc, char *argv[ ]){
 	}
 	
 	//Tratamento do input
-	scanf("%d %d %d", &n_grupos, &n_atores, &n_personagens);
-	printf("%d %d %d", n_grupos, n_atores, n_personagens);
-	
+	scanf("%d %d %d", &n_groups, &n_actors, &n_chars);
+	//alocando dinamicamente os atores
+	Actor actors = malloc(sizeof(Actor)*n_actors);
+	//passando por cada ator e definindo seus custos e grupos
+	for(i = 0; i < n_atores; i++){
+		int aux_n_groups
+		scanf("%d %d", &actors[i]->cost, &aux_n_groups);
+		&actors[i]->groups = malloc(sizeof(int)*aux_n_groups);
+		
+	}
 	return 0;
 }
 
