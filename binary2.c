c8302fe1d0fe51c458821a4fb5d2b4134de2a425
Jean Carlo Sanchuki Filho <jean.sanchuki@gmail.com>
2021-02-06 21:14:41 -0300

    Almost finished

--- binary2.c
+++ binary2.c
@@ -15,8 +15,7 @@ typedef struct node{
 	int level;
 	int nactors;
 	int actorIndex;
-	struct node *father, *left, *right;
-	struct node **son;
+	struct node *left, *right;
 	bool used;
 
 } Node;
@@ -42,15 +41,17 @@ typedef struct actorList{
 	
 } ActorList;
 
-bool isNotInPath(int *path, int actorIndex, int limit);
-float bound(Node *no, int ngroups, ActorList *list, int nchars);
-float newGroupTotal(bool *actual, Groups *newGroups);
-bool containGroup(Groups *groups, int need);
-bool isGroupsTrue(bool *groups, int size);
-bool isInAllGroups(int ngroups, int *list, int size, ActorList *actorL);
-int actorsInGroup(int *groupList, int size);
-int sumOfCosts(float *groups, int size);
+typedef struct stack{
+	
+	struct stack *next;
+	Node *no;
+	
+} Stack;
+
+void popAndStack(Stack **s, bool add);
 void printResult(int *path, int size, ActorList *list);
+float bound(Node *no, ActorList *list, bool left, bool a);
+bool isInAllGroups(int ngroups, int *list, int size, ActorList *actorL);
 
 int main(int argc, char *argv[ ]){
 		
@@ -58,7 +59,7 @@ int main(int argc, char *argv[ ]){
 	bool parameter_a = false;
 	int *all_groups;
 	int n_groups = 0, n_actors = 0, n_chars = 0;
-	int i,j;
+	int i,j, k;
 	bool *groupList;
 	//Verificação de argumentos
 	if(argc > 2)
@@ -102,243 +103,123 @@ int main(int argc, char *argv[ ]){
 		actorsList->actors[i] = actor;
 	}
 	
-	//Gerando a raiz da árvore
+	//Gerando a raiz da árvore e as variaveis necessarias
+	Stack *sa = malloc(sizeof(Stack));
+	sa->next = NULL;
+	sa->no = NULL;
+	int *reacopy = NULL;
+	Node *optimal = malloc(sizeof(Node));
+	optimal->cost = FLT_MAX;
 	Node *aux;
 	Node *root = malloc(sizeof(Node));
 	root->actorIndex = 0;
 	root->cost = 0;
-	root->father = NULL;
 	root->level = 1;
 	root->used = true;
 	root->nactors = 0;
 	root->path = NULL;
-	aux = root;
+	sa->no = root;
 	
-	while(aux->nactors != n_chars){
-		aux->left = malloc(sizeof(Node));
-		aux->right = malloc(sizeof(Node));
-		aux->left->father = aux;
-		aux->right->father = aux;
-		aux->left->nactors = aux->nactors + 1;
-		if(aux->left->path != NULL)
-			aux->left->path = NULL;
-		aux->left->path = realloc(aux->path, sizeof(int)*aux->left->nactors);
-		aux->left->level = aux->level + 1;
-		aux->left->path[aux->left->nactors - 1] = aux->left->level - 1;
-		aux->left->cost = bound(aux->left, n_groups, actorsList, n_chars);
-		printf("CUSTO ESQUERDA CALCULADO = %f", aux->left->cost);
-		
-		aux->right->nactors = aux->nactors;
-		if(aux->right->path != NULL)
-			aux->right->path = NULL;
-		aux->right->path = aux->path;
-		aux->right->level = aux->level + 1;
-		aux->right->cost = bound(aux->right, n_groups, actorsList, n_chars);
-		printf("CUSTO DIREITA CALCULADO = %f", aux->right->cost);
-		
-		if(aux->left->cost == -1){
-			if (aux->right->cost == -1){
-				printf("Inviavel");
-				return 0;
-			}
-			else{
-				aux = aux->son[i];
-			}
+	while(sa != NULL){
+		aux = sa->no;
+		if(aux->level == 11){
+			printf("AAAAAAAAAAAAAAA");
 		}
-		else if(aux->right->cost == -1){
-			aux = aux->left;
+		if(optimal->cost < aux->cost){
+		//	printf("REMOVENDO DA STACK\n");
+			popAndStack(&sa, false);
+		}
+		else if(aux->nactors == n_chars){
+			if(isInAllGroups(n_groups, aux->path, aux->nactors, actorsList)){
+				if(optimal->cost > aux->cost)
+					optimal = aux;
+			}
+			//	printf("REMOVENDO DA STACK\n");
+				popAndStack(&sa, false);
+		}
+		else if(aux->nactors + n_actors - (aux->level-1) < n_chars){
+			popAndStack(&sa, false);
 		}
 		else{
-			printf("E: %f\nD: %f\n", aux->left->cost, aux->right->cost);
-			if(aux->left->cost == aux->right->cost){
-				if (aux->left->nactors == n_chars){
-					if(isInAllGroups(n_groups, aux->path, aux->nactors, actorsList)){
-						aux = aux->left;
-						printf("Esquerda!\n");
-					}
-					else{
-						aux = aux->right;
-						printf("Direita!\n");
-					}
-					break;
-				}
-				aux = aux->left;
-					printf("Esquerda!\n");
+			aux->left = malloc(sizeof(Node));
+			aux->right = malloc(sizeof(Node));
+			if(aux == root){
+				aux->left->path = NULL;
+				aux->right->path = NULL;
 			}
-			else if(aux->left->cost < aux->right->cost){
-				aux = aux->left;
-				printf("Esquerda!\n");
-			}
-		 	else{
-		 		aux = aux->right;
-		 		printf("Direita!\n");
-			}
+			aux->right->nactors = aux->nactors;
+			aux->right->path = aux->path;
+			aux->right->level = aux->level + 1;
+			aux->right->cost = bound(aux, actorsList, false, parameter_a);
+			
+			aux->left->nactors = aux->nactors + 1;
+			aux->left->path = malloc(aux->left->nactors*sizeof(int));
+			aux->left->level = aux->level + 1;
+			for(k = 0; k < aux->nactors; aux->left->path[k] = aux->path[k], k++);
+			aux->left->path[aux->left->nactors - 1] = aux->left->level - 1;
+			aux->left->cost = bound(aux, actorsList, true, parameter_a);
+			//printf("ADICIONANDO A STACK\n");
+			popAndStack(&sa, true);
 		}
+		
 	}
 	
-	if(isInAllGroups(n_groups, aux->path, aux->nactors, actorsList)){
-		printResult(aux->path, aux->nactors, actorsList);
-		return 0;
+	if(optimal->cost == FLT_MAX){
+		printf("Inviavel");
 	}
-	printf("Inviavel");
+	else
+		printResult(optimal->path, n_chars, actorsList);
+	
 	return 0;
 }
 
+void popAndStack(Stack **s, bool add){
+	Stack *auxl = malloc(sizeof(Stack));
+	Stack *auxr = malloc(sizeof(Stack));
+	if(add){	
+		auxl->no = (*s)->no->left;
+		auxr->no = (*s)->no->right;
+		auxl->next = auxr;
+		auxr->next = (*s)->next;
+		*s = auxl;
+	}
+	else{
+		*s = (*s)->next;
+	}		
+}
+
 void printResult(int *path, int size, ActorList *list){
-	int i, sum;
+	int i, sum = 0;
 	
 	for(i = 0; i < size; i++){
 		printf("%d ", path[i]);
-		sum = list->actors[path[i]]->cost;
+		sum = sum + list->actors[path[i]-1]->cost;
 	}
 	printf("\n%d",sum);
-}
-
-float bound(Node *no, int ngroups, ActorList *list, int nchars){
 	
-	float vgroups[ngroups];
-	bool bgroups[ngroups];
-	int agroups[ngroups];
-	float menor;
-	int amenor;
-	int i, j;
-	float aux, aux1, aux2;
-	//Limpando lixo da memoria
-	for(i = 0; i < ngroups; i++){
-		vgroups[i] = FLT_MAX;
-		bgroups[i] = false;
-		agroups[i] = -1;
-	}
-	//calculo inicial com grupos do path
-	for(i = 0; i < ngroups; i++){
-		menor = FLT_MAX;
-		amenor = -1;
-		for(j = 0; j < no->nactors; j++){
-//			list->actors[no->path[j]]->groupsList[]
-			if(containGroup(list->actors[no->path[j]-1]->groupsList, i + 1)){
-				aux1 = list->actors[no->path[j]-1]->cost;
-				aux2 = pow(10, newGroupTotal(bgroups, list->actors[no->path[j]-1]->groupsList));
-				aux = aux1/aux2;
-				if(aux < menor){
-					menor = aux;
-					amenor = j;
-				}
-			}
-		}
-		
-		if(amenor != -1){
-			for(j = 0; j < list->actors[amenor]->groupsList->size; j++){
-				if(bgroups[list->actors[amenor]->groupsList->groups[j] - 1] == true){
-					if(list->actors[amenor]->cost < vgroups[list->actors[amenor]->groupsList->groups[j] - 1]){
-						vgroups[list->actors[amenor]->groupsList->groups[j] - 1] = list->actors[amenor]->cost;
-						agroups[list->actors[amenor]->groupsList->groups[j] - 1] = amenor;
-					}
-				}
-				else{
-					bgroups[list->actors[amenor]->groupsList->groups[j] - 1] = true;
-					vgroups[list->actors[amenor]->groupsList->groups[j] - 1] = list->actors[amenor]->cost;
-					agroups[list->actors[amenor]->groupsList->groups[j] - 1] = amenor;
-				}
-			}
-		}
-	}
+	return;
+}
+
+float bound(Node *no, ActorList *list, bool left, bool a){
+	if(a){
 	
-	//previsao de melhor grupo
-	for(i = 0; i < ngroups; i++){
-		menor = FLT_MAX;
-		amenor = -1;
-			for(j = no->level-1; j < list->size; j++){
-				if(containGroup(list->actors[j]->groupsList, i + 1)){
-					aux = list->actors[j]->cost/pow(10, newGroupTotal(bgroups, list->actors[j]->groupsList));
-					if(aux < menor){
-						menor = aux;
-						amenor = j;
-					}
-				}
-			}
-			if(amenor != -1){
-				for(j = 0; j < list->actors[amenor]->groupsList->size; j++){
-					if(bgroups[list->actors[amenor]->groupsList->groups[j] - 1] == true){
-						if(list->actors[amenor]->cost < vgroups[list->actors[amenor]->groupsList->groups[j] - 1]){
-							vgroups[list->actors[amenor]->groupsList->groups[j] - 1] = list->actors[amenor]->cost;
-							agroups[list->actors[amenor]->groupsList->groups[j] - 1] = amenor;
-						}
-					}
-					else{
-						bgroups[list->actors[amenor]->groupsList->groups[j] - 1] = true;
-						vgroups[list->actors[amenor]->groupsList->groups[j] - 1] = list->actors[amenor]->cost;
-						agroups[list->actors[amenor]->groupsList->groups[j] - 1] = amenor;
-					}
-				}
-			}
+		return 0;	
 	}
-	
-	if(isGroupsTrue(bgroups, ngroups)){
-		if(actorsInGroup(agroups, ngroups) <= nchars){
-			return sumOfCosts(vgroups, ngroups);
-		}
-		return -1;		
-	}
-	return -1;
-	
-}
-
-float newGroupTotal(bool *actual, Groups *newGroups){
-	int i;
-	float diff = 0;
-	for(i = 0; i < newGroups->size; i++){
-		if(actual[newGroups->groups[i]] == false)
-			diff += 1;
-	}
-	return diff;
-}
-
-bool containGroup(Groups *groups, int need){
-	int i;
-	for(i = 0; i < groups->size; i++){
-		if(groups->groups[i] == need)
-			return true;	
-	}
-	return false;
-}
-
-int sumOfCosts(float *groups, int size){
-	int total = 0, i;
-	for(i = 0; i < size; i++){
-		total += groups[i];
-	}
-	return total;
-}
-
-int actorsInGroup(int *groupList, int size){
-	bool listActors[size];
-	int i, total = 0;
-	for(i = 0; i < size; i++){
-		listActors[groupList[i]] = true;
-	}
-	for(i = 0; i < size; i++){
-		listActors[i] ? total++ : 0;
-	}
-	return total;
-}
-
-bool isGroupsTrue(bool *groups, int size){
-	int i;
-	for(i = 0; i < size; i++){
-		if(groups[i] == false)
-			return false;
-	}
-	return true;
+	//lazy bound
+	if(left)
+		return no->cost + list->actors[(no->left->level-1)-1]->cost;
+	return no->cost + list->actors[(no->right->level-1)-1]->cost;
 }
 
 bool isInAllGroups(int ngroups, int *list, int size, ActorList *actorL){
 	int i, j;
+	printResult(list, size, actorL);
 	//Checando se todos os grupos foram escolhido
 	bool groups[ngroups];
 	for(i = 0; i < ngroups; groups[i] = false, i++);
 	for(i = 0; i < size; i++){
-		for (j = 0; j < actorL->actors[list[i]]->groupsList->size; j++){
-			groups[actorL->actors[list[i]]->groupsList->groups[j]] = true;
+		for (j = 0; j < actorL->actors[list[i]-1]->groupsList->size; j++){
+			groups[actorL->actors[list[i]-1]->groupsList->groups[j]-1] = true;
 		}
 	}
 	for(i = 0; i < ngroups; i++){
@@ -348,12 +229,3 @@ bool isInAllGroups(int ngroups, int *list, int size, ActorList *actorL){
 	return true;
 }
 
-bool isNotInPath(int *path, int actorIndex, int limit){
-	int i;
-	bool flag = false;
-	for(i = 0; i < limit; i++){
-		if(path[i] == actorIndex)
-			flag = true;
-	}
-	return flag;
-}
