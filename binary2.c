8625c1f8257a8018b49a2ff6d2bf9c8904bc5613
Jean Carlo Sanchuki Filho <jean.sanchuki@gmail.com>
2021-02-08 21:15:23 -0300

    Fixed error message

--- binary2.c
+++ binary2.c
@@ -64,10 +64,10 @@ int main(int argc, char *argv[ ]){
 	int i,j, k;
 	bool *groupList;
 	//Verificação de argumentos
-	if(argc > 2)
+	if(argc > 2){
 		printf("O numero de argumentos e maior do que o esperado!\n");
 		printf("Continuando o programa supondo que o primeiro parametro seja -a\n");
-		
+	}
 	if(argc == 2){
 		if(strcmp(argv[1], "-a%0")){		
 			parameter_a = true;
